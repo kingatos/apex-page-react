96fe9794e52e6ad44618acf531467d0efe878780
Kinga Tos <toskinga@interia.pl>
2023-03-30 12:57:02 +0200

    created animation scroll to Menu

--- src/common/Menu/index.js
+++ src/common/Menu/index.js
@@ -1,10 +1,32 @@
-import { StyledMenu, StyledNavLink, ItemMenu,  } from "./styled";
+import { StyledMenu, StyledNavLink, ItemMenu } from "./styled";
+import { useEffect, useState } from "react";
+import HomeIcon from ".././Menu/HomeIcon/index";
 
 const Menu = () => {
+    const [menuHeight, setMenuHeight] = useState(100);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 0) {
+          setMenuHeight(50);
+        } else {
+          setMenuHeight(100);
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+  
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
 return (
-    <StyledMenu>
+    <StyledMenu menuHeight={menuHeight}>
         <ItemMenu>
-            <StyledNavLink to="/">Home</StyledNavLink>
+        <StyledNavLink to="/">
+          <HomeIcon />
+            </StyledNavLink>
         </ItemMenu>
         <ItemMenu>
             <StyledNavLink to="/o-firmie">O firmie</StyledNavLink>
