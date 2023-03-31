2c86a47c6bc7683bf1f96aafe066aad72bdf90be
Kinga Tos <toskinga@interia.pl>
2023-03-31 15:10:00 +0200

    all from menu navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Switch, Route  } from "react-router-dom";
 import Menu from "./common/Menu/index";
+import AboutPage from "./features/about/AboutPage";
+import HomePage from "./features/home/HomePage";
+import Footer from "./common/Footer";
+import OfferPage from "./features/offer/OfferPage";
+import Realizations from "./features/realizations/RealizationsPage";
+import ContactPage from "./features/contact/ContactPage";
 
 const App = () => (
 <HashRouter>
   <nav>
     <Menu />
-
+    <Switch> 
+      <Route path="/o-firmie">
+        <AboutPage />
+      </Route>
+      <Route path="/realizacje">
+        <Realizations />
+      </Route>
+      <Route path="/oferta">
+        <OfferPage />
+      </Route>
+      <Route path="/kontakt">
+        <ContactPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+    <Footer />
   </nav>
 </HashRouter>
 );
