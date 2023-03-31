0fe0f40c3e6ab250dfacab3c3eed85b01227383e
Kinga Tos <toskinga@interia.pl>
2023-03-31 15:10:16 +0200

    added the breakpoint to theme

--- src/theme.js
+++ src/theme.js
@@ -4,5 +4,12 @@ export const theme = {
         white: "#fff",
         swirl: "#D8CCC5",
         alto: "#D1D1D1",
-    }
+        rollingStone: "#787C82",
+        trout: "#4D535B", 
+        doveGray: "#696969",
+    },
+    breakpoint: {
+        mobileMax: 767,
+        mobileWidth: 320,
+    },
 }
