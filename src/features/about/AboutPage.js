3e6703da74b3052a30f26f2d1238cd8315b1e190
Kinga Tos <toskinga@interia.pl>
2023-03-31 15:10:49 +0200

    formatted AboutPage

--- src/features/about/AboutPage.js
+++ src/features/about/AboutPage.js
@@ -1,28 +1,82 @@
-import {
-  StyledContainer,
-  ImageWrapper,
-  TextWrapper,
-} from "../../common/Container/styled";
+import { StyledContainer } from "../../common/Container/styled";
 import Header from "../../common/Header/index";
 import Section from "../../common/Section/index";
-import Img3 from "../../images/wiertarka.jpg";
+import SectionGray from "../../common/SectionGray";
+import { ImageWrapper, TextWrapper } from "../../common/Section/styled";
 
 const AboutPage = () => (
   <StyledContainer>
     <Header title="O firmie" />
-    <Section img={<ImageWrapper img={Img3} />} body={<TextWrapper>
-        <h2>Solidność, doświadczenie, profesjonalizm</h2>
-        <p>
-          Zakład Remontowo-Budowlany EDBUD to firma z wieloletnim doświadczeniem
-          na rynku budowlanym, kompleksowo realizująca projekty remontowe oraz
-          modernizacyjne. Nasze usługi obejmują m.in. remonty mieszkań, domów
-          jednorodzinnych oraz lokali użytkowych, a także modernizacje obiektów
-          przemysłowych. Działamy szybko i sprawnie, zapewniając najwyższą
-          jakość wykonywanych prac oraz konkurencyjne ceny. Zaufaj nam i
-          przekonaj się, że remonty mogą być łatwe i przyjemne!
-        </p>
-    </TextWrapper>
-     }
+    <Section
+      body={
+        <>
+          <ImageWrapper>
+            <img
+              src="./images/wiertarka.jpg"
+              alt="wiercenie ściany"
+              width="500"
+              height="500"
+            />
+          </ImageWrapper>
+          <TextWrapper>
+            <h2>Solidność, doświadczenie, profesjonalizm</h2>
+            <p>
+              Zakład Remontowo-Budowlany EDBUD to firma z wieloletnim
+              doświadczeniem na rynku budowlanym, kompleksowo realizująca
+              projekty remontowe oraz modernizacyjne. Nasze usługi obejmują
+              m.in. remonty mieszkań, domów jednorodzinnych oraz lokali
+              użytkowych, a także modernizacje obiektów przemysłowych. Działamy
+              szybko i sprawnie, zapewniając najwyższą jakość wykonywanych prac
+              oraz konkurencyjne ceny. Zaufaj nam i przekonaj się, że remonty
+              mogą być łatwe i przyjemne!
+            </p>
+          </TextWrapper>
+        </>
+      }
+    />
+    <SectionGray
+      body={
+        <>
+          <TextWrapper>
+            <h2>Budujemy dla Ciebie, z pasją i zaangażowaniem</h2>
+            <p>
+              Ponadto, w naszej ofercie znajduje się tynkowanie, zakładanie
+              stolarki budowlanej, posadzkarstwo, malowanie i szklenie oraz
+              wykonanie pozostałych robót budowlanych wykończeniowych. Jesteśmy
+              również specjalistami w zakresie konstrukcji i pokryć dachowych, a
+              także oferujemy usługi w zakresie inżynierii i doradztwa
+              technicznego.
+            </p>
+          </TextWrapper>
+          <ImageWrapper>
+            <img
+              src="./images/gladz.jpg"
+              alt="rusztowanie"
+              width="500"
+              height="500"
+            />
+          </ImageWrapper>
+        </>
+      }
+    />
+    <Section
+      body={
+        <>
+          <ImageWrapper>
+            <img src="./images/pracawre.jpg" alt="wiercenie ściany" />
+          </ImageWrapper>
+          <TextWrapper>
+            <h2>Twoje marzenia, nasza pasja</h2>
+            <p>
+              Nasza firma kładzie nacisk na jakość i terminowość realizowanych
+              prac. Zatrudniamy tylko doświadczonych fachowców, którzy potrafią
+              sprostać nawet najbardziej wymagającym zadaniom. Dzięki temu nasi
+              Klienci mogą być pewni, że ich inwestycje będą wykonane solidnie i
+              zgodnie z najwyższymi standardami.
+            </p>
+          </TextWrapper>
+        </>
+      }
     />
   </StyledContainer>
 );
