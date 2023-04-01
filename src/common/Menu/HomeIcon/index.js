8c7e39f50f5f84e9aa45038b4794e1fec990c23c
Kinga Tos <toskinga@interia.pl>
2023-04-01 17:24:31 +0200

    added HomeIcon

---
+++ src/common/Menu/HomeIcon/index.js
@@ -0,0 +1,9 @@
+import { StyledHomeIcon } from "./styled";
+
+const HomeIcon = () => {
+    return (
+      <StyledHomeIcon src="./images/ZRB EDBUD_1.png" alt="home" />
+      );
+  };
+  
+  export default HomeIcon;
