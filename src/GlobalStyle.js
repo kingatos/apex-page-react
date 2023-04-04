3ba5b37f0966aa3a432a4a737e67f1c4f8fa988e
Kinga Tos <toskinga@interia.pl>
2023-04-04 23:08:12 +0200

    removed padding bottom

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,7 +12,7 @@ html {
 body {
     font-family: 'Montserrat', sans-serif;
     margin: 0 auto;
-    padding: 20px 0;
+    padding: 20px 0px 0px;
     background-color: ${({ theme }) => theme.color.white};
     word-break: break-word;
     overflow-wrap: break-word;
