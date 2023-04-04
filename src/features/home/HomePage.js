849816bdca28c7b52798b281bcc0011504f50621
Kinga Tos <toskinga@interia.pl>
2023-04-04 22:09:59 +0200

    modificated homepage code

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,51 +1,101 @@
-import { StyledContainer, ImageWrapper, TextWrapper } from "../../common/Container/styled";
-import Header from "../../common/Header";
-import SectionGrey from "../../common/SectionGrey";
-import Section from "../../common/Section";
+import { StyledContainer } from "../../common/Container/styled";
+import {
+  WrapperSectionGreyHomePage,
+  SectionGreyBodyHomePage,
+  TextWrapperHomePage,
+  ImageWrapperHomePage,
+  WrapperSectionHomePage,
+  SectionBodyHomePage,
+  SectionQuote,
+  SectionQuoteHeader,
+  SectionQuoteWrapper,
+  SectionQuoteTestimonials,
+  SectionQuoteArcicle,
+  SectionQuoteAuthor,
+  SectionQuoteContent,
+} from "./styled";
 import Button from "./Button";
-import Img1 from "../../images/wiertarka.jpg";
-import Img2 from "../../images/cienie.jpg";
 
 const HomePage = () => (
   <StyledContainer>
-    <Header title="O firmie" />
-    <SectionGrey
-      img={Img1}
-      body={
+    <SectionGreyBodyHomePage>
+      <WrapperSectionGreyHomePage>
         <>
-          <TextWrapper>
+          <TextWrapperHomePage>
             <h2>Budujemy dla Ciebie, z pasją i zaangażowaniem</h2>
             <p>
-              Zakład Remontowo-Budowlany EDBUD to firma z ponad 30-letnim
-              doświadczeniem na rynku budowlanym. Zajmujemy się kompleksowymi
-              usługami remontowo-budowlanymi, w tym robotami związanymi z
-              wykończeniem budynków mieszkalnych i niemieszkalnych, rozbiórką i
-              burzeniem obiektów budowlanych, instalacjami elektrycznymi i
-              wodno-kanalizacyjnymi, tynkowaniem, stolarką budowlaną, malowaniem
-              oraz wykonaniem pozostałych robót budowlanych wykończeniowych.
+              <strong>Zakład Remontowo-Budowlany EDBUD</strong> to firma z ponad
+              30-letnim doświadczeniem na rynku budowlanym. Zajmujemy się
+              kompleksowymi usługami remontowo-budowlanymi, w tym robotami
+              związanymi z wykończeniem budynków mieszkalnych i niemieszkalnych,
+              rozbiórką i burzeniem obiektów budowlanych, instalacjami
+              elektrycznymi i wodno-kanalizacyjnymi, tynkowaniem, stolarką
+              budowlaną, malowaniem oraz wykonaniem pozostałych robót
+              budowlanych wykończeniowych.
             </p>
-          </TextWrapper>
+          </TextWrapperHomePage>
         </>
-      }
-    />
+      </WrapperSectionGreyHomePage>
+    </SectionGreyBodyHomePage>
 
-    <Section>
-      <ImageWrapper>img={Img2}</ImageWrapper>
-      body=
-      {
-        <>
-          <TextWrapper>
-            <h2>Solidność, doświadczenie, profesjonalizm</h2>
-            <p>
-              Jesteśmy firmą, która stawia na solidność, jakość i
-              profesjonalizm, a nasza praca jest zawsze zgodna z najwyższymi
-              standardami.
-            </p>
-            <Button>Zobacz więcej</Button>
-          </TextWrapper>
-        </>
-      }
-    </Section>
+    <SectionQuote>
+      <SectionQuoteHeader>Opinie</SectionQuoteHeader>
+
+      <SectionQuoteWrapper>
+        <SectionQuoteTestimonials>
+          <SectionQuoteArcicle>
+            <SectionQuoteAuthor>Przemysław W.</SectionQuoteAuthor>
+            <SectionQuoteContent>
+              Świetna firma. Od lat korzystam z usług i nigdy mnie nie zawiedli,
+              zawsze solidnie i uczciwie. Jeśli chcesz mieć wykonaną fachową
+              robotę to gorąco ich polecam.
+            </SectionQuoteContent>
+          </SectionQuoteArcicle>
+
+          <SectionQuoteArcicle>
+            <SectionQuoteAuthor>Piotr S.</SectionQuoteAuthor>
+            <SectionQuoteContent>
+              "Elegancja-francja", fachowa robota bardzo dokładna. Jestem
+              zadowolony.
+            </SectionQuoteContent>
+          </SectionQuoteArcicle>
+
+          <SectionQuoteArcicle>
+            <SectionQuoteAuthor>Dariusz G.</SectionQuoteAuthor>
+            <SectionQuoteContent>
+              W 100% polecam. Firma godna zaufania, remont mieszkania wykonany
+              bardzo precyzyjnie, szybko, wedle moich upodobań, wzbogacony o
+              innowacyjne pomysły właściciela. Wykonanie po przystępnej cenie.
+              Brak jakichkolwiek zarzutów.
+            </SectionQuoteContent>
+          </SectionQuoteArcicle>
+
+          <SectionQuoteArcicle>
+            <SectionQuoteAuthor>Katarzyna T.</SectionQuoteAuthor>
+            <SectionQuoteContent>
+              Super firma polecam z całego serca, praca super wykonana i szybko.
+              Znają się na robocie.
+            </SectionQuoteContent>
+          </SectionQuoteArcicle>
+        </SectionQuoteTestimonials>
+      </SectionQuoteWrapper>
+    </SectionQuote>
+
+    <WrapperSectionHomePage>
+      <SectionBodyHomePage>
+        <ImageWrapperHomePage>
+          <img src="./images/cienie.jpg" alt="efekt" />
+        </ImageWrapperHomePage>
+        <TextWrapperHomePage>
+          <h2>Solidność, doświadczenie, profesjonalizm</h2>
+          <p>
+            Jesteśmy firmą, która stawia na solidność, jakość i profesjonalizm,
+            a nasza praca jest zawsze zgodna z najwyższymi standardami.
+          </p>
+          <Button>Zobacz więcej</Button>
+        </TextWrapperHomePage>
+      </SectionBodyHomePage>
+    </WrapperSectionHomePage>
   </StyledContainer>
 );
 
