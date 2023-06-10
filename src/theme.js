ef4bb01efa49772fbaeefba696c0770ca9ee876c
Kinga Tos <toskinga@interia.pl>
2023-06-10 19:26:10 +0200

    formatted code

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,18 @@
 export const theme = {
-    color: {
-        black: "#000",
-        white: "#fff",
-        swirl: "#D8CCC5",
-        alto: "#D1D1D1",
-        rollingStone: "#787C82",
-        trout: "#4D535B", 
-        doveGray: "#696969",
-        charade: "#2E2D37",
-        dixie: "#e08e16",
-    },
-    breakpoint: {
-        mobileMax: 768,
-        mobileMin: 320,
-        mobileHigh: 1023,
-    },
-}
+  color: {
+    black: "#000",
+    white: "#fff",
+    swirl: "#D8CCC5",
+    alto: "#D1D1D1",
+    rollingStone: "#787C82",
+    trout: "#4D535B",
+    doveGray: "#696969",
+    charade: "#2E2D37",
+    dixie: "#e08e16",
+  },
+  breakpoint: {
+    mobileMax: 768,
+    mobileMin: 320,
+    mobileHigh: 1023,
+  },
+};
