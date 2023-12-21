009cbf5e0bf96df5d67daf328eb168b1de22e5ce
Kinga Tos <toskinga@interia.pl>
2023-12-21 23:44:53 +0100

    formatted code

--- src/common/SectionGray/index.js
+++ src/common/SectionGray/index.js
@@ -1,12 +1,12 @@
 import { WrapperSectionGray, SectionGrayBody } from "./styled";
 
 const SectionGray = ({ img, body }) => (
-    <WrapperSectionGray>
-        <SectionGrayBody>
-            {img}
-            {body}
-        </SectionGrayBody>
-    </WrapperSectionGray>
+  <WrapperSectionGray>
+    <SectionGrayBody>
+      {img}
+      {body}
+    </SectionGrayBody>
+  </WrapperSectionGray>
 );
 
 export default SectionGray;
