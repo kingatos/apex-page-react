40bcd840e85289dc68f4bf72e6f13f296f6c05f7
Kinga Tos <toskinga@interia.pl>
2024-02-28 22:03:10 +0100

    added socialMedia.js file to footer

---
+++ src/common/Footer/socialMedia.js
@@ -0,0 +1,28 @@
+
+import { StyledIcon } from "./styled";
+import { ReactComponent as facebookIcon } from "./icons/facebookIcon.svg";
+import { ReactComponent as instagramIcon } from "./icons/instagramIcon.svg";
+import { ReactComponent as linkedinIcon } from "./icons/linkedinIcon.svg";
+
+const facebook = "https://www.facebook.com/";
+const linkedin = "https://www.linkedin.com/in/";
+const instagram = "https://www.instagram.com/";
+
+
+export const socialMedia = [
+  {
+    name: "facebook",
+    link: facebook,
+    Icon: StyledIcon(facebookIcon),
+  },
+  {
+    name: "linkedin",
+    link: linkedin,
+    Icon: StyledIcon(linkedinIcon),
+  },
+  {
+    name: "instagram",
+    link: instagram,
+    Icon: StyledIcon(instagramIcon),
+  },
+];
