342a4fed60f25dac47266aee5d1dd1281eeb04c6
Kinga Tos <toskinga@interia.pl>
2024-05-14 18:29:24 +0200

    added padding and minimalized width

--- src/common/Menu/HomeIcon/styled.js
+++ src/common/Menu/HomeIcon/styled.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const StyledHomeIcon = styled.img`
   display: block;
-  width: 200px;
+  width: 100px;
+  padding: 10px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    width: 160px;
+    width: 100px;
   }
 `;
